abc928ea387d4f8100bdfd7a741600f1c9e98f3e
Alexandre Moreau <contact@alexandre-moreau.fr>
2018-07-14 12:11:22 +0200

    Finalisation grammaire

--- src/turingmachine/grammar.hpp
+++ src/turingmachine/grammar.hpp
@@ -8,27 +8,33 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+enum direction {LEFT = true, RIGHT = false};
+using state = std::string;
 
 class Grammar {
 public:
     Grammar();
+    Grammar(std::string name);
     ~Grammar();
-    void addRule(std::string currentState, std::string read, std::string newState, std::string write, bool direction);
+    void addRule(state currentState, std::string read, state newState, std::string write, direction direction);
+    void addFinalState(state finalState);
     void listRuleDisplay();
 private:
+
    struct Rule {
-        std::string currentState;    // état de la machine lors de la lecture du caractère
+        state currentState;         // état de la machine lors de la lecture du caractère
 
-        std::string read;            // caractère lu sur la bande
+        std::string read;           // caractère lu sur la bande
 
-        std::string newState;        // état de la machine après l'écriture du caractère
+        state newState;             // état de la machine après l'écriture du caractère
 
-        std::string write;           // caractère écrit sur la bande
+        std::string write;          // caractère écrit sur la bande
 
-        bool direction;             // direction du déplacement
+        direction direction;        // direction du déplacement
     };
     std::vector<Rule> rulesList;
     std::string name;
+    state finalState;
 };
 
 #endif //MACHINETURING_RULES_HPP
